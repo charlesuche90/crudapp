1c3d6281adc3764e1bf992ebc6c85c0c49acebf6
Charles Uche <charlexi90@gmail.com>
2021-07-08 06:57:38 -0700

    first commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Crud Application in Laravel</title>
+@extends('posts.layout')
+  
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('posts.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="{{ route('posts.store') }}" method="POST">
+    @csrf
+  
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Introduction:</strong>
+                <input type="text" name="introduction" class="form-control" placeholder="Enter introduction">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Location:</strong>
+                <input type="text" name="location" class="form-control" placeholder="Enter Location">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Cost:</strong>
+                <input type="text" name="cost" class="form-control" placeholder="Enter Cost">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,48 @@
+@extends('posts.layout')
+ 
+@section('content')
+    <div class="row" style="margin-top: 5rem;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Report Application made with Crud Laravel : charlesuche.com.ng</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('posts.create') }}"> Add New Report</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Introduction</th>
+            <th>Location</th>
+            <th>Cost</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($data as $key => $value)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $value->introduction }}</td>
+            <td>{{ \Str::limit($value->location, 100) }}</td>
+            <td>{{ \Str::limit($value->cost, 100) }}</td>
+            <td>
+                <form action="{{ route('posts.destroy',$value->id) }}" method="POST">   
+                    <a class="btn btn-info" href="{{ route('posts.show',$value->id) }}">Show</a>    
+                    <a class="btn btn-primary" href="{{ route('posts.edit',$value->id) }}">Edit</a>   
+                    @csrf
+                    @method('DELETE')      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>  
+    {!! $data->links() !!}      
+@endsection
